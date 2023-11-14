851b388f44c5eed10bdceb54b3f5ded66e559796
Roista57 <Roista57@gmail.com>
2023-11-14 16:24:28 +0900

    feat: 주문할 메뉴와 메뉴의 개수를 입력받는 기능 추가

--- src/main/java/christmas/ChristmasSystem.java
+++ src/main/java/christmas/ChristmasSystem.java
@@ -1,6 +1,10 @@
 package christmas;
 
 
+import org.mockito.internal.junit.UniversalTestListener;
+
+import java.util.ArrayList;
+
 public class ChristmasSystem {
     private final InputView inputView;
     private final OutputView outputView;
@@ -17,6 +21,9 @@ public class ChristmasSystem {
         outputView.printMessage(Util.Message.STARTMESSAGE.getMessage());
         outputView.printMessage(Util.Message.VISIT.getMessage());
         int orderDay = inputVisitErrorCheck();
+
+        outputView.printMessage(Util.Message.ORDER.getMessage());
+        ArrayList<Menu> menus = order();
     }
 
     public int inputVisitErrorCheck(){
@@ -39,5 +46,28 @@ public class ChristmasSystem {
         if (day < START || day > END){
             throw new IllegalArgumentException(Util.Error.ERROR_DAY.getMessage());
         }
+    }
+
+    public ArrayList<Menu> order(){
+        ArrayList<Menu> menus = new ArrayList<>();
+        boolean check = false;
+
+        while(!check){
+            try{
+                menus = orderCheck(menus);
+                check = true;
+            }catch(IllegalArgumentException e){
+                outputView.printMessage(e.getMessage());
+            }
+        }
+        return menus;
+    }
+
+    public ArrayList<Menu> orderCheck(ArrayList<Menu> menus){
+        String orderMenu = inputView.readLine();
+        for(String menu : orderMenu.split(",")){
+            menus.add(new Menu(menu.split(("-"))));
+        }
+        return menus;
     }
 }
--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -3,16 +3,6 @@ package christmas;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private enum Message{
-        VISIT("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        private final String message;
-        Message(String message){
-            this.message = message;
-        }
-        public String getMessage(){
-            return message;
-        }
-    }
     public int inputVisit(){
         try{
             String day = Console.readLine();
@@ -20,5 +10,9 @@ public class InputView {
         }catch(NumberFormatException e){
             throw new IllegalArgumentException(Util.Error.ERROR_DAY.getMessage());
         }
+    }
+
+    public String readLine(){
+        return Console.readLine();
     }
 }
