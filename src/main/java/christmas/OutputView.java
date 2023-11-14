db65c1f5c812acbd04afb22324564486ef0bf6c5
Roista57 <Roista57@gmail.com>
2023-11-14 12:57:58 +0900

    feat: 시작 메시지 출력 기능 추가

---
+++ src/main/java/christmas/OutputView.java
@@ -0,0 +1,20 @@
+package christmas;
+
+public class OutputView {
+    private enum Message{
+        STARTMESSAGE("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        private final String message;
+
+        Message(String message){
+            this.message = message;
+        }
+
+        public String getMessage(){
+            return message;
+        }
+
+    }
+    public void startMessage(){
+        System.out.println(Message.STARTMESSAGE);
+    }
+}
